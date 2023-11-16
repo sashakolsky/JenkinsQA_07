549d8200ba5c4b722cde5fba575d7fbbe52cfbd7
Denis Prokofyev <denisprokofyev@gmail.com>
2023-11-16 18:00:51 +0300

    Rf tc 05.002.08 10 (#1746)
    * RF_05.002.08-10 | Add dependencies and change locators
    
    * TC_05.002.08-10 | EC import

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
