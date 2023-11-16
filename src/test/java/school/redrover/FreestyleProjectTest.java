5209b6e49220620a751a1f58d782a8fc885e1d7c
jokerSharp <137673317+jokerSharp@users.noreply.github.com>
2023-11-16 14:41:05 +0300

    TC_01.002.17 | testRenameProject was moved to common FreestyleProjectTest file (#1739)
    testRenameProject renamed to testRenameProjectFromDashboard

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -1025,4 +1025,22 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertTrue(notificationIsDisplayed.contains("--visible"));
     }
+
+    @Test
+    public void testRenameProjectFromDashboard() {
+        final String projectName = "FSproject";
+        final String projectRename = "FSproject1";
+
+        createFreeStyleProject(projectName);
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
+        hoverClick("//*[@id='job_" + projectName + "']/td[3]/a");
+        hoverClick("//a[@href='/job/" + projectName + "/confirm-rename']");
+        getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
+        hoverClickInput("//input[@name='newName']", projectRename);
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='" + projectRename + "']")).isDisplayed());
+    }
 }
