b94196accbf8969c03732d143d27174d81c5a951
EkaterinaRogozhnikova <134730100+ekaterinarogozhnikova@users.noreply.github.com>
2023-11-17 07:49:57 +0300

    RF_ Folder > Delete_class/add_Create_Folder_POM (#1756)
    * RF_ Folder > Delete_class/add_Create_Folder_POM
    
    * RF_ Folder > Delete_class/add_Create_Folder_POM
    
    * RF_ Folder > Delete_class/add_Create_Folder_POM
    
    * RF_ Folder > Delete_class/add_Create_Folder_POM
    
    * RF_ Folder > Delete_class/add_Create_Folder_POM

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -6,22 +6,15 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.model.HomePage;
 import school.redrover.runner.BaseTest;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.testng.AssertJUnit.assertEquals;
 
-
 public class FolderTest extends BaseTest {
-
     private static final String FOLDER_NAME = "Folder";
     private static final String FOLDER_NAME_2 = "My new project";
-    private static final String VALID_NAME = "Folder1";
-    private static final String EMPTY_NAME = "";
-    private static final String INVALID_NAME = ".";
-    private static final String NEW_VALID_NAME = "Folder2";
     private static final String NAME_FOR_BOUNDARY_VALUES = "A";
 
     private void creationNewFolder(String folderName) {
@@ -43,29 +36,8 @@ public class FolderTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
     }
 
-    private void utilsCreate(String folderName) {
-        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']")).click();
-        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(folderName);
-        getDriver().findElement(By.xpath("//button[@type = 'submit']")).click();
-        getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
-    }
-
     private void getDashboardLink() {
         getDriver().findElement(By.xpath("//li/a[@href='/']")).click();
-    }
-
-    private void utilsGoDashboard() {
-        getDriver().findElement(By.xpath("//img[@alt = 'Jenkins']")).click();
-    }
-
-    private void createFolderAddReturnToDashboard(String folderName) {
-        getDriver().findElement(By.className("task-link")).click();
-        getDriver().findElement(By.id("name")).sendKeys(folderName);
-        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
-        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
     }
 
     private WebElement findJobByName(String name) {
@@ -77,19 +49,13 @@ public class FolderTest extends BaseTest {
         getDriver().findElement(By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']")).click();
     }
 
-    private void creatNewFolder(String folderName) {
+    private void create(String folderName) {
 
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.id("name")).sendKeys(folderName);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
-    }
-
-    private void utilsDelete(String folderName) {
-        getDriver().findElement(By.xpath("//span[text() = '" + folderName + "']")).click();
-        getDriver().findElement(By.xpath("//a[@href = '/job/" + folderName + "/delete']")).click();
-        getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
     }
 
     @Test
@@ -135,34 +101,14 @@ public class FolderTest extends BaseTest {
 
     @Ignore
     @Test
-    public void testRenameWithInvalidName() {
-        final String oldFolderName = "Old folder";
-        final String invalidFolderName = "*";
-
-        creationNewFolder(oldFolderName);
-
-        getDashboardLink();
-
-        getDriver().findElement(By.xpath("//*[@id='job_" + oldFolderName + "']/td[3]/a")).click();
-        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[7]/span/a")).click();
-
-        getDriver().findElement(By.name("newName")).clear();
-        getDriver().findElement(By.name("newName")).sendKeys(invalidFolderName);
-        getDriver().findElement(By.name("Submit")).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/p")).getText(), "‘" + invalidFolderName + "’ is an unsafe character");
-    }
-
-    @Ignore
-    @Test
     public void TestMoveFolder() {
         final String firstFolderName = "Original Folder";
         final String secondFolderName = "Inserted Folder";
 
-        creatNewFolder(firstFolderName);
+        create(firstFolderName);
         getDriver().findElement(By.linkText("Dashboard")).click();
 
-        creatNewFolder(secondFolderName);
+        create(secondFolderName);
         getDriver().findElement(By.linkText("Dashboard")).click();
 
         getDriver().findElement(By.xpath("//*[@id='job_" + secondFolderName + "']/td[3]/a")).click();
@@ -178,39 +124,6 @@ public class FolderTest extends BaseTest {
     }
 
     @Ignore
-    @Test
-    public void testCreatingNewFolder() {
-        final String folderName = "TestFolder";
-
-        getDriver().findElement(By.xpath("//*[@href='newJob']")).click();
-
-        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys(folderName);
-        getDriver().findElement(By.xpath("//img[@class='icon-folder icon-xlg']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
-
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//*[@class='jenkins-table__link model-link inside']")).getText(),
-                folderName);
-
-    }
-
-    @Test
-    public void testCreatingNewFolder1() {
-        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a")).click();
-        getDriver().findElement(By.xpath("//*[@id=\"name\"]")).sendKeys(FOLDER_NAME_2);
-        getDriver().findElement(By.xpath("//*[@id=\"j-add-item-type-nested-projects\"]/ul/li[1]")).click();
-        getDriver().findElement(By.xpath("//*[@id=\"ok-button\"]")).click();
-        getDriver().findElement(By.xpath("//*[@id=\"bottom-sticker\"]/div/button[1]"));
-
-        getDriver().findElement(By.xpath("//*[@id=\"breadcrumbs\"]/li[1]/a")).click();
-        Assert.assertEquals
-                (getDriver().findElement(By.xpath("//span[text()='My new project']")).getText(),
-                        FOLDER_NAME_2);
-
-    }
-
     @Test
     public void testRenameFolderUsingBreadcrumbDropdownOnFolderPage() {
 
@@ -254,6 +167,7 @@ public class FolderTest extends BaseTest {
         Assert.assertTrue(okButtonDisabled, "OK button is clickable when it shouldn't be!");
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testCreatingNewFolder1")
     public void testAddDisplayName() {
         final String folderDisplayName = "Best folder";
@@ -323,44 +237,6 @@ public class FolderTest extends BaseTest {
     }
 
     @Test
-    public void testCreateEmptyName() {
-        utilsGoNameField();
-
-        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(EMPTY_NAME);
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id = 'itemname-required']")).getText(), "» This field cannot be empty, please enter a valid name");
-    }
-
-    @Test
-    public void testCreateValidName() {
-        utilsGoNameField();
-
-        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(VALID_NAME);
-        getDriver().findElement(By.xpath("//button[@type = 'submit']")).click();
-
-        utilsGoDashboard();
-        boolean findName = getDriver().findElement(By.xpath("//tr[@id = 'job_" + VALID_NAME + "']")).isDisplayed();
-
-        Assert.assertTrue(findName);
-    }
-
-    @Test
-    public void testRename() {
-        utilsCreate(VALID_NAME);
-
-        getDriver().findElement(By.xpath("//a[@href = '/job/" + VALID_NAME + "/confirm-rename']")).click();
-        getDriver().findElement(By.xpath("//input[@value = '" + VALID_NAME + "']")).clear();
-
-        getDriver().findElement(By.xpath("//input[@checkurl = 'checkNewName']")).sendKeys(NEW_VALID_NAME);
-        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
-
-        utilsGoDashboard();
-        boolean findName = getDriver().findElement(By.xpath("//tr[@id = 'job_" + NEW_VALID_NAME + "']")).isDisplayed();
-
-        Assert.assertTrue(findName);
-    }
-
-    @Test
     public void testCreateNameSpecialCharacters() {
         List<String> invalidNames = Arrays.asList("#", "&", "?", "!", "@", "$", "%", "^", "*", "|", "/", "\\", "<", ">", "[", "]", ":", ";");
 
@@ -379,17 +255,6 @@ public class FolderTest extends BaseTest {
     }
 
     @Test
-    public void testDelete() {
-        utilsCreate(VALID_NAME);
-        utilsGoDashboard();
-        utilsDelete(VALID_NAME);
-
-        getDriver().findElement(By.xpath("//input[@role = 'searchbox']")).sendKeys(VALID_NAME + "\n");
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'error']")).getText(), "Nothing seems to match.");
-    }
-
-    @Test
     public void testBoundaryValuesName() {
         utilsGoNameField();
 
@@ -397,14 +262,14 @@ public class FolderTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[@type = 'submit']")).click();
         getDriver().findElement(By.xpath("//h1[text() = 'Configuration']"));
 
-        utilsGoDashboard();
+        getDashboardLink();
         utilsGoNameField();
 
         getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(NAME_FOR_BOUNDARY_VALUES.repeat(255));
         getDriver().findElement(By.xpath("//button[@type = 'submit']")).click();
         getDriver().findElement(By.xpath("//h1[text() = 'Configuration']"));
 
-        utilsGoDashboard();
+        getDashboardLink();
         utilsGoNameField();
 
         getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(NAME_FOR_BOUNDARY_VALUES.repeat(256));
